f3773a06cb3a44efc18aea3bb1367c2b6301ab7b
eliyahubelay0@gmail.com <eliyahubelay0@gmail.com>
2022-08-09 23:37:07 +0300

    been trying some basic components and some of them proprties

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedBack, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
+  // const handllePressOnText = ()=>{
+  //   console.log('text was pressed');
+  // }
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>
+        Hello React Native
+      </Text>
+      <TouchableNativeFeedback>
+        <View style={{width:200, height:70, backgroundColor:'dodgerblue'}}></View>
+        {/* <Image
+          source={
+            {
+              uri: "http://picsum.photos/200/300",
+              width: 200,
+              height: 300
+            }}
+          blurRadius={5}
+          fadeDuration={700}>
+        </Image> */}
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // backgroundColor: 'rgb(145, 6, 131)',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
